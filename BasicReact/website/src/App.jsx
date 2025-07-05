d5cbd25b3a59d9e151e414a775b7850c5a20998e
istekhar44 <67890istekharalam@gmail.com>
2025-07-05 12:51:19 +0530

    commit

--- BasicReact/website/src/App.jsx
+++ BasicReact/website/src/App.jsx
@@ -1,34 +1,22 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import React from 'react'
+import UserCard from './Components/UserCard.jsx'
+import men from './assets/men.jpeg'
+import article from './assets/article.jpg'
+import black from './assets/black.jpeg'
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
 
+const App = () => {
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div className="container" >
+      <UserCard name='Maharana' desc="hellow ji kasa ho sara" image={men}/>
+      <UserCard name='RanaPratap'desc="Ma hu hero" image={article}/>
+      <UserCard name="kuwarSingh"desc="I am millioner" image={black}/>
+      
+
+
+      
+    </div>
   )
 }
 
